c7bae2ea285cee1cf94bffbc3c54ddf228979058
Sudip Pokhrel <106923052+sudippokhrel@users.noreply.github.com>
2023-01-23 22:41:06 +0545

    Objects connected by id's

--- app/src/main/java/com/example/dictionary/MainActivity.java
+++ app/src/main/java/com/example/dictionary/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.dictionary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    SearchView search_view;
+    TextView tvWord;
+    RecyclerView rvPhonetics ,rvMeanings;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        search_view = findViewById(R.id.search_view);
+        tvWord = findViewById(R.id.tvWord);
+        rvPhonetics = findViewById(R.id.rvPhonetics);
+        rvMeanings = findViewById(R.id.rvMeanings);
     }
 }
