8142f1012d4cea59796484f9b428d6bc0ea1297a
Sudip Pokhrel <106923052+sudippokhrel@users.noreply.github.com>
2023-02-01 17:20:15 +0545

    UI improved

--- app/src/main/java/com/example/dictionary/MainActivity.java
+++ app/src/main/java/com/example/dictionary/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.dictionary;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.dictionary.Adapters.MeaningAdapter;
 import com.example.dictionary.Adapters.PhoneticsAdapter;
@@ -17,75 +17,90 @@ import com.example.dictionary.Models.APIResponse;
 public class MainActivity extends AppCompatActivity {
     SearchView search_view;
     TextView tvWord;
-    RecyclerView rvPhonetics ,rvMeanings;
+    RecyclerView rvPhonetics,rvMeanings;
     ProgressDialog progressDialog;
     PhoneticsAdapter phoneticsAdapter;
     MeaningAdapter meaningAdapter;
+    //Adapter phoneticsAdapter,meaningAdapter;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        search_view=findViewById(R.id.search_view);
+        tvWord=findViewById(R.id.tvWord);
+        rvPhonetics=findViewById(R.id.rvPhonetics);
+        //  Recyclerview_phonetics.setAdapter(phoneticsAdapter);
 
-        search_view = findViewById(R.id.search_view);
-        tvWord = findViewById(R.id.tvWord);
-        rvPhonetics = findViewById(R.id.rvPhonetics);
-        rvMeanings = findViewById(R.id.rvMeanings);
-        progressDialog = new ProgressDialog(this);
-
-        progressDialog.setTitle("Loading");
+        rvMeanings=findViewById(R.id.rvMeanings);
+        // rvMeanings.setAdapter(meaningAdapter);
+        progressDialog=new ProgressDialog(this);
+        //loads this query by default  every time the application is run
+        progressDialog.setTitle("Loading data...");
         progressDialog.show();
+        RequestManager manager=new RequestManager(MainActivity.this);
+        manager.getWordMeaning(listner,"hello");
 
-//        to display hello world searched by default
-        RequestManager manager = new RequestManager(MainActivity.this);
-        manager.getWordMeaning(listener, "hello");
-
+        //handles the query submitted by the user
         search_view.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public boolean onQueryTextSubmit(String s) {
-                progressDialog.setTitle("Fetching response for" + s);
+            public boolean onQueryTextSubmit(String query) {
+                progressDialog.setTitle("Fetching response for: "+ query);
                 progressDialog.show();
-                RequestManager manager = new RequestManager(MainActivity.this);
-                manager.getWordMeaning(listener,s);
+
+                RequestManager manager=new RequestManager(MainActivity.this);
+                manager.getWordMeaning(listner,query);
+
+
                 return true;
             }
 
             @Override
-            public boolean onQueryTextChange(String s) {
+            public boolean onQueryTextChange(String newText) {
                 return false;
             }
         });
+
     }
-    private final OnFetchDataListener listener = new OnFetchDataListener() {
+    private final OnFetchDataListener listner=new OnFetchDataListener() {
         @Override
         public void onFetchData(APIResponse apiResponse, String message) {
             progressDialog.dismiss();
             if(apiResponse==null){
-                Toast.makeText(MainActivity.this, "No Data found!!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Data not found!!", Toast.LENGTH_SHORT).show();
                 return;
+
             }
             showData(apiResponse);
+
         }
 
         @Override
         public void onError(String message) {
             progressDialog.dismiss();
-            Toast.makeText(MainActivity.this, message , Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+
 
         }
     };
 
     private void showData(APIResponse apiResponse) {
-        tvWord.setText("Word: " + apiResponse.getWord());
+        tvWord.setText("Word:" + apiResponse.getWord());
         rvPhonetics.setHasFixedSize(true);
+
         rvPhonetics.setLayoutManager(new GridLayoutManager(this,1));
-        phoneticsAdapter = new PhoneticsAdapter(this, apiResponse.getPhonetics());
+        phoneticsAdapter=new PhoneticsAdapter(this,apiResponse.getPhonetics());
         rvPhonetics.setAdapter(phoneticsAdapter);
 
+
+
         rvMeanings.setHasFixedSize(true);
-        rvMeanings.setLayoutManager(new GridLayoutManager(this, 1));
-        meaningAdapter = new MeaningAdapter(this, apiResponse.getMeanings());
+        rvMeanings.setLayoutManager(new GridLayoutManager(this,1));
+        meaningAdapter=new MeaningAdapter(this,apiResponse.getMeanings());
         rvMeanings.setAdapter(meaningAdapter);
+
+
     }
 }
